ddbbfbfe9a9fb6d74be9f163246909049ad10467
cheetah <ahmedwested2000@gmail.com>
2025-03-26 00:56:58 +0300

    commit 2

--- src/components/BestCards.jsx
+++ src/components/BestCards.jsx
@@ -1,4 +1,3 @@
-import Arrwo from '../../public/icons/Arrow up.svg'
 import Image from 'next/image'
 export const BestCards = ({ Best }) => {
   return (
@@ -7,7 +6,7 @@ export const BestCards = ({ Best }) => {
         <h1 className=" text-white font-noto font-bold text-[24px]">{Best.branch}</h1>
         <h1 className=" text-white mt-4 font-noto font-semibold text-[19px]">{Best.department}</h1>
         <h1 className=" text-white font-noto font-regular text-[15px]">{Best.university}</h1>
-        <div className=" mt-3 flex"><h1 className=" text-white font-noto underline font-medium text-[18px]">تعرف للمزيد</h1><Image className=" w-5 h-5 ml-2 mt-1" alt="" src={Arrwo} /></div>
+        <div className=" mt-3 flex"><h1 className=" text-white font-noto underline font-medium text-[18px]">تعرف للمزيد</h1></div>
       </div>
       <Image width={1} height={1} className=" w-full h-[100%] object-cover" alt="" loading='lazy' src={Best.imageUrl} /></div>
   )
