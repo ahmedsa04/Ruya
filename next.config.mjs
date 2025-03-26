315d7ce42a90b6aff94a3bf26cfc7e3d86543a57
cheetah <ahmedwested2000@gmail.com>
2025-03-26 08:36:23 +0300

    test

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,12 @@ import { withNextVideo } from "next-video/process";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: {
+        remotePatterns: [{
             host: "lcokgog8osc8wco84sso08wc.194.163.168.91.sslip.io",
             port: "",
             protocol: "http",
         }
+        ]
     }
     
 };
